25643c7c98c50caac5ff7fcdd29c3813fe12b772
VANROYE Victorien <victorien.vanroye@gmail.com>
2014-09-16 20:19:57 +0200

    - Add Style For Arm3Configs

---
+++ BPCLauncherA3/BPCLauncherA3/Arma3Configs.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BPCLauncherA3
+{
+    /// <summary>
+    /// Logique d'interaction pour Arma3Configs.xaml
+    /// </summary>
+    public partial class Arma3Configs
+    {
+        private MainWindow mwindow;
+        public Arma3Configs(MainWindow mw)
+        {
+            this.mwindow = mw;
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Closed(object sender, EventArgs e)
+        {
+            this.mwindow.CallBack_Close_Arma3Configs();
+        }
+
+        private void Arma3Configs_Abord(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
--- BPCLauncherA3/BPCLauncherA3/MainWindow.xaml.cs
+++ BPCLauncherA3/BPCLauncherA3/MainWindow.xaml.cs
@@ -27,38 +27,87 @@ namespace BPCLauncherA3
 
         private String userEmail = "";
         private String userPassword = "";
+        private String userToken = "";
         //
         private Boolean isA3Connected = false;
         private Boolean isServerConnected = false;
         private Boolean isAuthentified = false;
         //
         private Boolean checkUptodateIncomming = false;
-        private Boolean Uptodate = false;
+
+        // 0 : not check
+        // 1 : need Authentification
+        // 2 : check incomming
+        // 4 : check all ok
+        // 5 : downloading maj
+        private int Uptodate = 0;
         //
         private Boolean arma3Running = false;
+        //
+        private Arma3Configs cfgwindow;
+        private Boolean cfgwindow_open = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
         private void title_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
         }
 
-        private void Launcher_Close(object sender, RoutedEventArgs e)
+        //-- Setter Logique
+        public void setUptodate(int set)
         {
-            this.Close();
+            switch (set)
+            {
+                case 0:
+                    this.Uptodate = 0;
+                    this.Launcher_Label_Uptodate.Content = "Uptodate : non vérifier";
+                    break;
+                case 1:
+                    this.Uptodate = 1;
+                    this.Launcher_Label_Uptodate.Content = "Uptodate : need Authentification";
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
         }
+
+        //-- CallBack des Fenêtres
+
+        public void CallBack_Close_Arma3Configs(){
+            this.cfgwindow_open = false;
+            this.cfgwindow = null;
+        }
+
+        public void CallBack_Authenticate_Login(String email, String password, String token)
+        {
+            this.userEmail = email;
+            this.userPassword = password;
+            this.userToken = token;
+            this.isAuthentified = true;
+            if (!this.arma3Running)
+            {
+                this.Button_Launch_ProjetX.IsEnabled = true;
+                this.Button_Launch_ProjetX.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("White"));
+                this.Button_Launch_ProjetX.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF39B00"));
+            }
+        }
+
+        //-- Logique Allumage Arma
 
         private void Launcher_StartA3(object sender, RoutedEventArgs e)
         {
             if (!this.arma3Running)
             {
                 Console.WriteLine("XX- Lancement de Arma3 Vanilla");
-                this.arma3Running = true;
                 this.Running_Arma3();
-                this.NotRunning_Arma3();
             }
             else
             {
@@ -71,7 +120,6 @@ namespace BPCLauncherA3
             if (!this.arma3Running)
             {
                 Console.WriteLine("XX- Lancement de Arma3 FrenchLife");
-                this.arma3Running = true;
                 this.Running_Arma3();
             }
             else
@@ -85,7 +133,6 @@ namespace BPCLauncherA3
             if (!this.arma3Running)
             {
                 Console.WriteLine("XX- Lancement de Arma3 Projet X");
-                this.arma3Running = true;
                 this.Running_Arma3();
                 
             }
@@ -129,8 +176,8 @@ namespace BPCLauncherA3
             if (this.isAuthentified)
             {
                 this.Button_Launch_ProjetX.IsEnabled = true;
-                this.Button_Launch_ProjetX.Foreground = SystemColors.ControlDarkBrush;
-                this.Button_Launch_ProjetX.Background = SystemColors.ControlLightBrush;
+                this.Button_Launch_ProjetX.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("White"));
+                this.Button_Launch_ProjetX.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF39B00"));
             }
             else
             {
@@ -139,5 +186,37 @@ namespace BPCLauncherA3
                 this.Button_Launch_ProjetX.Background = SystemColors.ControlLightBrush;
             }
         }
+
+        //-- Logique des Menus
+
+        private void Launcher_Site_FrenchLife(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://french-life.eu");
+        }
+
+        private void Launcher_Site_ProjetX(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://nordri.fr");
+        }
+
+        private void Launcher_Options_Arma3(object sender, RoutedEventArgs e)
+        {
+            if (!this.cfgwindow_open)
+            {
+                this.cfgwindow_open = true;
+                this.cfgwindow = new Arma3Configs(this);
+                this.cfgwindow.Show();
+                this.cfgwindow.Focus();
+            }
+            else
+            {
+                this.cfgwindow.Show();
+                this.cfgwindow.Focus();
+            }
+        }
+
+        //-- Logique de Mise à jour
+
+        //-- Logique d'Authentification
     }
 }
