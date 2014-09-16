8711b317160415dbd6654c2818fbc7e29434e5cb
VANROYE Victorien <victorien.vanroye@gmail.com>
2014-09-16 16:37:25 +0200

    New Compile

--- BPCLauncherA3/BPCLauncherA3/Login.xaml.cs
+++ BPCLauncherA3/BPCLauncherA3/Login.xaml.cs
@@ -11,17 +11,49 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace BPCLauncherA3
 {
     /// <summary>
     /// Logique d'interaction pour Login.xaml
     /// </summary>
-    public partial class Login : Window
+    public partial class Login
     {
-        public Login()
+        private MainWindow main;
+        private bool isNormalClose = false;
+        public Login(MainWindow mw)
         {
+            this.main = mw;
             InitializeComponent();
         }
+
+        private void Login_Abord(object sender, RoutedEventArgs e)
+        {
+            this.Login_Abord();
+        }
+
+        private void Login_Abord()
+        {
+            this.isNormalClose = true;
+            this.Close();
+            this.main.Show();
+        }
+
+        private void Login_Login(object sender, RoutedEventArgs e)
+        {
+            this.isNormalClose = true;
+
+        }
+
+        private void Login_Closed(object sender, EventArgs e)
+        {
+            if (!this.isNormalClose)
+            {
+                this.Login_Abord();
+            }
+        }
+
+        
     }
 }
--- BPCLauncherA3/BPCLauncherA3/MainWindow.xaml.cs
+++ BPCLauncherA3/BPCLauncherA3/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using MahApps.Metro.Controls;
 
+
 namespace BPCLauncherA3
 {
     /// <summary>
@@ -24,14 +25,20 @@ namespace BPCLauncherA3
 
     {
 
+        private String userEmail = "";
+        private String userPassword = "";
+        //
+        private Boolean isA3Connected = false;
+        private Boolean isServerConnected = false;
+        private Boolean isAuthentified = false;
+        //
+        private Boolean checkUptodateIncomming = false;
+        private Boolean Uptodate = false;
+        //
+        private Boolean arma3Running = false;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Launcher_Close(object sender, RoutedEventArgs e)
-        {
-            this.Close();
         }
 
         private void title_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -39,9 +46,98 @@ namespace BPCLauncherA3
             DragMove();
         }
 
+        private void Launcher_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
         private void Launcher_StartA3(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("XX- Lancement de Arma3");
+            if (!this.arma3Running)
+            {
+                Console.WriteLine("XX- Lancement de Arma3 Vanilla");
+                this.arma3Running = true;
+                this.Running_Arma3();
+                this.NotRunning_Arma3();
+            }
+            else
+            {
+                //Arma3 Alredy Running Force Closing and Start This or Abord
+            }
+        }
+
+        private void Launcher_StartA3_FrenchLife(object sender, RoutedEventArgs e)
+        {
+            if (!this.arma3Running)
+            {
+                Console.WriteLine("XX- Lancement de Arma3 FrenchLife");
+                this.arma3Running = true;
+                this.Running_Arma3();
+            }
+            else
+            {
+                //Arma3 Alredy Running Force Closing and Start This or Abord
+            }
+        }
+
+        private void Launcher_StartA3_ProjetX(object sender, RoutedEventArgs e)
+        {
+            if (!this.arma3Running)
+            {
+                Console.WriteLine("XX- Lancement de Arma3 Projet X");
+                this.arma3Running = true;
+                this.Running_Arma3();
+                
+            }
+            else
+            {
+                //Arma3 Alredy Running Force Closing and Start This or Abord
+            }
+        }
+
+        private void Launcher_StartLogin(object sender, RoutedEventArgs e)
+        {
+            Login loginWindow = new Login(this);
+            this.Hide();
+            loginWindow.Show();
+        }
+
+        private void Running_Arma3()
+        {
+            this.arma3Running = true;
+            this.Button_Launch_Arma3.IsEnabled = false;
+            this.Button_Launch_Arma3.Foreground = SystemColors.ControlDarkBrush;
+            this.Button_Launch_Arma3.Background = SystemColors.ControlLightBrush;
+            this.Button_Launch_FrenchLife.IsEnabled = false;
+            this.Button_Launch_FrenchLife.Foreground = SystemColors.ControlDarkBrush;
+            this.Button_Launch_FrenchLife.Background = SystemColors.ControlLightBrush;
+            this.Button_Launch_ProjetX.IsEnabled = false;
+            this.Button_Launch_ProjetX.Foreground = SystemColors.ControlDarkBrush;
+            this.Button_Launch_ProjetX.Background = SystemColors.ControlLightBrush;
+        }
+
+        private void NotRunning_Arma3()
+        {
+            this.arma3Running = false;
+            this.Button_Launch_Arma3.IsEnabled = true;
+            this.Button_Launch_Arma3.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("White"));
+            this.Button_Launch_Arma3.Background = SystemColors.HighlightBrush;
+            this.Button_Launch_FrenchLife.IsEnabled = true;
+            this.Button_Launch_FrenchLife.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("White"));
+            this.Button_Launch_FrenchLife.Background = SystemColors.HotTrackBrush;
+            
+            if (this.isAuthentified)
+            {
+                this.Button_Launch_ProjetX.IsEnabled = true;
+                this.Button_Launch_ProjetX.Foreground = SystemColors.ControlDarkBrush;
+                this.Button_Launch_ProjetX.Background = SystemColors.ControlLightBrush;
+            }
+            else
+            {
+                this.Button_Launch_ProjetX.IsEnabled = false;
+                this.Button_Launch_ProjetX.Foreground = SystemColors.ControlDarkBrush;
+                this.Button_Launch_ProjetX.Background = SystemColors.ControlLightBrush;
+            }
         }
     }
 }
