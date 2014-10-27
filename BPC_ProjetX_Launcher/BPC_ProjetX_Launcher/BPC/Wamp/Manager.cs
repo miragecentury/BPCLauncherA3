228c79b7a6d7739756cf6922b74d5e5b5e7b44e1
VANROYE Victorien <victorien.vanroye@gmail.com>
2014-10-27 23:36:15 +0100

    - New Project for refacto

---
+++ BPC_ProjetX_Launcher/BPC_ProjetX_Launcher/BPC/Wamp/Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPC_ProjetX_Launcher.BPC.Wamp
+{
+    class Manager
+    {
+    }
+}
---
+++ BPC_ProjetX_Launcher/BPC_ProjetX_Launcher/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BPC_ProjetX_Launcher
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void setStatusServerStatus(bool status){
+            if(status){
+                this.MainWindow_Status_ServeurStatus.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Green"));
+            }else{
+                this.MainWindow_Status_ServeurStatus.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Red"));
+            }
+        }
+
+        public void setStatusServerConnectionStatus(bool status)
+        {
+            if (status)
+            {
+                this.MainWindow_Status_ServeurConnectionStatus.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Green"));
+            }
+            else
+            {
+                this.MainWindow_Status_ServeurConnectionStatus.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Red"));
+            }
+        }
+
+        public void setStatusServeurAuthentification(bool status)
+        {
+            if (status)
+            {
+                this.MainWindow_Status_ServeurAuthentification.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Green"));
+            }
+            else
+            {
+                this.MainWindow_Status_ServeurAuthentification.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Red"));
+            }
+        }
+
+        public void setStatusGameLinkConnection(bool status)
+        {
+            if (status)
+            {
+                this.MainWindow_Status_GameLinkConnection.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Green"));
+            }
+            else
+            {
+                this.MainWindow_Status_GameLinkConnection.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Red"));
+            }
+        }
+    }
+}
